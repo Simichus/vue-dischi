fe93b181cc4046e88c444f88056826698a7dd175
Simichus <simonefilippobilardo@gmail.com>
2021-09-02 14:39:56 +0200

    aggiunto filtro

--- js/script.js
+++ js/script.js
@@ -6,6 +6,23 @@ const app = new Vue({
   el: "#app",
   data: {
     discs: [],
+    filter: "all",
+  },
+  computed: {
+    filterByGenre() {
+      const filter = this.filter;
+      const filteredDiscs = this.discs.filter(
+        (disc) => disc.genre === filter || filter === "all"
+      );
+      return filteredDiscs;
+    },
+    genres() {
+      const genres = [];
+      this.discs.forEach((disc) => {
+        if (!genres.includes(disc.genre)) genres.push(disc.genre);
+      });
+      return genres;
+    },
   },
   methods: {},
   created() {
